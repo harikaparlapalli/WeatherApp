5cd436a3f412cd0005998172c5c64531378c34a1
harikaparlapalli <24wh1a05c8@bvrithyderabad.edu.in>
2026-04-16 15:38:56 +0530

    Updated weather app with API and chart

---
+++ script.js
@@ -0,0 +1,66 @@
+const apiKey = "8afcddee9194d0e1971bb1aa9d12be8c";
+let chart;
+// Arrow Function + Async/Await
+
+const getWeather = async () => {
+  const city = document.getElementById("city").value;
+  if (!city) {
+    alert("Please enter a city name");
+    return;
+  }
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
+    );
+    const data = await response.json();
+    console.log(data);
+    // Handle API errors
+    if (data.cod !== "200") {
+      alert("Error: " + data.message);
+      return;
+    }
+    processWeather(data, displayGraph);
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Something went wrong!");
+  }
+};
+// Callback Function
+const processWeather = (data, callback) => {
+  const temps = data.list.slice(0, 8).map(item => item.main.temp);
+  const labels = data.list.slice(0, 8).map(item => item.dt_txt);
+  callback(labels, temps);
+};
+// Promise example (ES6 concept)
+
+const fakePromise = () => {
+  return new Promise(resolve => {
+    resolve("Promise executed");
+  });
+};
+fakePromise().then(console.log);
+// Graph display
+const displayGraph = (labels, temps) => {
+  const ctx = document.getElementById("weatherChart").getContext("2d");
+  //  Destroy old chart
+  if (chart) chart.destroy();
+  chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "Temperature (°C)",
+        data: temps,
+        borderWidth: 2,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "blue",
+        fill: true,
+        tension: 0.3
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+};
